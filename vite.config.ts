d375dcf9caa73fc0a77ab086ba750a6e442ad4ec
蔡朝勃 <3279338858@qq.com>
2024-11-21 15:28:12 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import dtsPlugin from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './lib/main.ts',
+      name: 'AxiosInterceptor',
+      fileName: 'axios-interceptor',
+    },
+    target: 'es2015',
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    dtsPlugin({
+      outDir: './dist/types',
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
